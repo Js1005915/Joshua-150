f95e62b2bd2149a0220fb7beab9faad5a6ad02da
Js1005915 <js1005915@otc.edu>
2025-02-06 09:15:13 -0600

    first and last push, i hate this file and everything about it. happy Hannukah

---
+++ Exam 1 - Student Template-1/Exam 1 - Student Template/Exam 1 - Higher or Lower/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.Metrics;
+using System.Linq.Expressions;
+
+namespace LargestNumberFinder
+{
+    public class NumberProgram
+    {
+        static void Main(string[] args)
+        {
+            int largest = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                ProcessInput(Console.ReadLine(), ref largest);
+            }
+
+        }
+
+        public static int ProcessInput(string input, ref int largest)
+        {
+            Console.Write("Please enter a number");
+            input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+    }
+}
