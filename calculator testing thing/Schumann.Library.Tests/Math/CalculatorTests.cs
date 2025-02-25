3f90d65f5aec665d2e9af9b37e40aa089f0f45d4
Js1005915 <js1005915@otc.edu>
2025-02-25 09:52:05 -0600

    calculator test, incomplete

---
+++ calculator testing thing/Schumann.Library.Tests/Math/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schumann.Library.Tests.Math
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+
+        //arrange
+        Calculator calcc = new Calculator();
+
+
+        //naming: MethodName_Scenario_Expectedoutcome
+        [TestMethod]
+        public void Add_AddsToWholeNumbers_SumOfThoseTwoNumbers()
+        {
+            //act 
+            var result = calcc.Add(5, 7);
+
+            //assert
+            Assert.AreEqual(12, result);
+        }
+    }
+}
